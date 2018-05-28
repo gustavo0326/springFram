d8f579fe94d8f0d11d9c7243f38a28ddb66d1fd1
unknown <gustavo0326@gmail.com>
2018-05-27 20:16:48 -0500

    creacion

---
+++ src/main/java/com/example/demo/PrimerosPApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerosPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerosPApplication.class, args);
+	}
+}
